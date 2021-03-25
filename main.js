6e4a2268fa20886cf0ec5c71db4264db8e46138a
jeskyncino <mail@helakonecna.cz>
2021-03-25 23:44:39 +0100

    It works!:)

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-// toto budeš potřebovat později
-/*
-if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	// panacek a mince se prekryvaji
-}
-*/
+
+// Panáček parametry
 let panacek = document.querySelector("#panacek");
 let panacekSirka = 64 ;
 let panacekVyska = 70 ;
-let panacekX = parseInt(panacek.style.left);
-let panacekY = parseInt(panacek.style.top);
 
 let panacekXS = "500px" ;
 let panacekYS = "500px" ;
 panacek.style.left = panacekXS;
 panacek.style.top = panacekYS;
 
+// Okno parametry
 let windowWidth = window.innerWidth ;
 let windowHeight = window.innerHeight ;
 
+// Mince parametry
 mince = document.querySelector("#mince");
 let minceSirka = 36;
 let minceVyska = 36;
 mince.style.top = (Math.floor(Math.random() * (windowHeight - 36))) + "px";
 mince.style.left = (Math.floor(Math.random() * (windowWidth - 36))) + "px";
-let minceX = parseInt(mince.style.left);
-let minceY = parseInt(mince.style.top);
 
+function play (event) {
+	if (event.key == "ArrowDown" || event.key == "ArrowUp" || event.key == "ArrowLeft" || event.key == "ArrowRight" ) {
+		hudba.play(); 
+	} else {} 
+}
 
-function go(event, step) {
-	hudba.play();    
+function go (event, step) {   
 	if (event.key == "ArrowDown" && ((parseInt(panacek.style.top) + panacekVyska) < (windowHeight - step))) {
 		panacek.style.top = parseInt(panacek.style.top) + step + "px" ;
 		panacek.src = "obrazky/panacek.png";
@@ -39,7 +37,7 @@ function go(event, step) {
 		zjistiStav()
 	} else if (event.key == "ArrowLeft" && (parseInt(panacek.style.left) > step)) {
 		panacek.style.left = parseInt(panacek.style.left) - step + "px" ;
-		panacek.src = "obrazky/panacek-vlevo.png";
+		panacek.src = "obrazky/panacek-vlevo.png";	
 		zjistiStav()
 	} else if (event.key == "ArrowRight" && ((parseInt(panacek.style.left) + panacekSirka) < (windowWidth - step))) {
 		panacek.style.left = parseInt(panacek.style.left) + step + "px" ;
@@ -47,10 +45,28 @@ function go(event, step) {
 		zjistiStav()
 	} else {}
 }
+
+
 function zjistiStav () {
+	let panacekX = parseInt(panacek.style.left);
+	let panacekY = parseInt(panacek.style.top);
+	let minceX = parseInt(mince.style.left);
+	let minceY = parseInt(mince.style.top);
+	let score = document.querySelector("#score");
 	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	} else { 
-	};
-	console.log(!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY));
-	//console.log(panacekX < minceX && (panacekX + panacekSirka) > (minceX + minceSirka) && panacekY < minceY && (minceY + minceVyska) < (panacekY + panacekVyska));
+		document.querySelector("#zvukmince").play();
+		mince.style.top = (Math.floor(Math.random() * (windowHeight - 36))) + "px";
+		mince.style.left = (Math.floor(Math.random() * (windowWidth - 36))) + "px";
+		score.innerHTML = parseInt(score.innerHTML) + 1;
+		zjistiVyhru()
+	} else {};
+}
+
+function zjistiVyhru () {
+	if (score.innerHTML === "5" ) {
+		hudba.pause()
+		document.querySelector("#zvukfanfara").play();
+		alert("Gratuluji! Vyhrál jsi!")
+		document.querySelector("body").removeAttribute("onkeyup")
+	} else {}
 }
